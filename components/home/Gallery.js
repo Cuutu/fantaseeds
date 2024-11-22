2368cbfa705c30cc03bb802f90f7eb4bf1f52ff7
Cuutu <franco.l.varela99@gmail.com>
2024-11-21 22:17:36 -0300

    feat: update gallery images with high quality cannabis photos

--- components/home/Gallery.js
+++ components/home/Gallery.js
@@ -1,10 +1,8 @@
-'use client';
-
 import { useState } from 'react';
 import Image from 'next/image';
 import Slider from 'react-slick';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
-import ScrollReveal from '@/components/animations/ScrollReveal';
+import ScrollReveal from '../ScrollReveal';
 
 // Importar los estilos de slick
 import "slick-carousel/slick/slick.css";
@@ -15,28 +13,28 @@ export default function Gallery() {
   
   const images = [
     {
-      src: "/images/weed1.webp",
+      src: "https://i.imgur.com/xJbzgw1.png",
       alt: "Instalaciones del Club"
     },
     {
-      src: "/images/weed2.webp",
+      src: "https://i.imgur.com/8I9VORo.png",
       alt: "Cultivo Indoor"
     },
     {
-      src: "/images/weed3.webp",
+      src: "https://i.imgur.com/OCngmuA.png",
       alt: "Terpenos"
     },
     {
-      src: "/images/weed4.webp",
+      src: "https://i.imgur.com/UEDUqYQ.png",
       alt: "Calidad"
     },
     {
-      src: "/images/weed5.webp",
+      src: "https://i.imgur.com/fNPmV5y.png",
       alt: "Cultivo"
     },
     {
-      src: "/images/weed6.webp",
-      alt: "Instalaciones del Club"
+      src: "https://i.imgur.com/GdDHWBa.png",
+      alt: "Instalaciones del "
     }
   ];
 
