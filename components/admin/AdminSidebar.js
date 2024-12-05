20851cb80dacd5b34c030fa7a9a11c2317d294bb
Cuutu <franco.l.varela99@gmail.com>
2024-12-05 03:29:26 -0300

    Versión final

--- components/admin/AdminSidebar.js
+++ components/admin/AdminSidebar.js
@@ -9,7 +9,7 @@ const AdminSidebar = () => {
   return (
     <div className="bg-gray-800 text-gray-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
       <div className="flex items-center space-x-2 px-4">
-        <span className="text-2xl font-extrabold text-green-500">FANTASEEDS</span>
+        <span className="text-2xl font-extrabold text-green-500">FANTASEEDS.</span>
       </div>
 
       <nav className="space-y-2">
